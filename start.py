05e9cf0f684f6577f65b378e54ff3636eefe0b4c
Andrew Collier <andrew@fathomdata.dev>
2024-11-12 06:05:30 +0000

    fix: Strip trailing period

--- start.py
+++ start.py
@@ -23,6 +23,7 @@ def get_versions():
         version = result.stdout.decode("utf-8").partition("\n")[0]
         version = re.sub(r" +([0-9/]{10})?[ -]*\(?(https://.*)?\)?\.?$", "", version)
         version = re.sub(r" version", ":", version)
+        version = re.sub("\.$", "", version)
 
         log.info("- " + version)
 
